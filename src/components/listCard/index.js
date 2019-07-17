16c2299c519bdb4f2016848bf64ffdf51ce8727e
davidpaley <david.paleyy@gmail.com>
2019-07-17 16:55:18 -0300

    completing card item elements

--- src/components/listCard/index.js
+++ src/components/listCard/index.js
@@ -8,11 +8,24 @@ class ListCard extends PureComponent {
     data: PropTypes.object.isRequired,
   };
 
+  getHoursAgo = (date) => {
+    const postDate = new Date(date*1000);
+    const today = new Date();
+    const diffTime = Math.abs(today.getTime() - postDate.getTime());
+    const diffHours = Math.ceil(diffTime / (1000 * 60 * 60)); 
+    console.log(diffHours);
+    return diffHours;
+  }
+
   render() {
     const { data } = this.props;
     return (
       <div className="list-card">
-        <h5 className="list-title">{data.author}</h5>
+        <div className="title-container">
+          <h5 className="list-title">{data.author}</h5>
+          <span className="hours-container">{this.getHoursAgo(data.created)} hours ago</span>
+
+        </div>
         <div className="container">
           <div className="img-container">
             {data.thumbnail &&
@@ -24,6 +37,13 @@ class ListCard extends PureComponent {
           <div className="arrow">
             >
           </div>
+        </div>
+        <div className="title-container">
+          <button type="button" className="button-dismiss">
+            Dismiss post
+          </button>
+          <span className="num-comments">{data.num_comments} comments</span>
+
         </div>
         <hr className="card-division" />
       </div>
